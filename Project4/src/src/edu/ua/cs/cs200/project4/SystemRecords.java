1bfc342594b7cbbc1a960540b64dd983b382336d
dddierking <dddierking@crimson.ua.edu>
2023-11-08 19:54:54 -0600

    System Records

---
+++ Project4/src/src/edu/ua/cs/cs200/project4/SystemRecords.java
@@ -0,0 +1,18 @@
+package edu.ua.cs.cs200.project4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SystemRecords {
+  public ProviderRecords providerRecords;
+  public MemberRecords memberRecords;
+  public List<EFTRecords> eftRecords;
+  public List<SummaryReport> summaryReports;
+  public List<MemberReport> memberReports;
+  
+  public SystemRecords() {
+    eftRecords = new ArrayList<EFTRecords>();
+    summaryReports = new ArrayList<SummaryReport>();
+    memberReports = new ArrayList<MemberReport>();
+  }
+}
