6400b8517b34693db7d31c96c1105ee29dd966ba
rmmilsap <rmmilsap@crimson.ua.edu>
2023-11-08 19:54:51 -0600

    initial commit - milsap

---
+++ Project4/src/src/edu/ua/cs/cs200/project4/ProviderTerminal.java
@@ -0,0 +1,21 @@
+package edu.ua.cs.cs200.project4;
+
+public class ProviderTerminal {
+	  
+
+    public boolean validateMember(int memberID) {
+        //  validate a member
+        // Return true if valid, false otherwise
+        return true; 
+    }
+
+    public void billService(int memberID, int serviceCode) {
+        //  bill a service for a member
+    }
+
+    public void requestDirectory() {
+        //request the provider directory
+    }
+
+
+}
