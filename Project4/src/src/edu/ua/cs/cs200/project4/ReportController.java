0045287ef09e2c079f5f8aa0d4cc4222dc714bb3
JTCaldwell <jtcaldwell2@crimson.ua.edu>
2023-11-08 21:13:35 -0600

    Report update

--- Project4/src/src/edu/ua/cs/cs200/project4/ReportController.java
+++ Project4/src/src/edu/ua/cs/cs200/project4/ReportController.java
@@ -4,10 +4,14 @@ public class ReportController {
 
 	public generateManagerReport() {
 		//open selected report
+			//select report type
+				//select which report
+					//?generate?
 	}
 	
 	public generateAllReports() {
 		//open all reports individuallly
+			//repeat steps as above, for each report
 	}
 	
 }
