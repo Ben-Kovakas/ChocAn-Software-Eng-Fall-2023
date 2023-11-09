1a213c51cffa26534eb3f92adc055569463ab849
sirconnerhall <cchall5@crimson.ua.edu>
2023-11-08 20:28:59 -0600

    expanding base skeletons of project

--- Project4/src/src/edu/ua/cs/cs200/project4/MainMenu.java
+++ Project4/src/src/edu/ua/cs/cs200/project4/MainMenu.java
@@ -3,26 +3,36 @@ package edu.ua.cs.cs200.project4;
 
 public class MainMenu {
 	
+	//class objects
+	OperatorTerminal operatorTerminal;
+    ManagerTerminal managerTerminal;
+    ProviderTerminal providerTerminal;
+    Timer timer;
+    SystemRecords systemRecords;
+	
+	public static void main(String[] args) {
+		//TODO - fill in main method
+    }
+	
 	public void initialize() {
-    	
+    	//TODO - logic to initialize program
     }
 
     public void shutdown() {
-    	
+    	//TODO - logic to shutdown program - persistence?
     }
 
     public boolean login(int IDNumber, String password) {
+    	//TODO - login logic
+    	
+    	
+    	
         return false;
     }
 
     public void logout() {
+    	//TODO - logout logic
     	
     }
-
-    public OperatorTerminal operatorTerminal;
-    public ManagerTerminal managerTerminal;
-    public ProviderTerminal providerTerminal;
-    public Timer timer;
-    public SystemRecords systemRecords;
     
 }
--- Project4/src/src/edu/ua/cs/cs200/project4/Timer.java
+++ Project4/src/src/edu/ua/cs/cs200/project4/Timer.java
@@ -1,8 +1,12 @@
 package edu.ua.cs.cs200.project4;
 
-/**
- * @
- */
 public class Timer {
-
+	
+	public void RunProcedure() {
+		
+		ReportController reportController = new ReportController();
+		reportController.generateAllReports();
+		
+	}
+	
 }
