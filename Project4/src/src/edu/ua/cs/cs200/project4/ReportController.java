67e79c07c23a5e41d9ecd59d0da9f01b3c68dbc6
dddierking <dddierking@crimson.ua.edu>
2023-11-09 10:55:17 -0600

    change method type to void

--- Project4/src/src/edu/ua/cs/cs200/project4/ReportController.java
+++ Project4/src/src/edu/ua/cs/cs200/project4/ReportController.java
@@ -2,14 +2,14 @@ package edu.ua.cs.cs200.project4;
 
 public class ReportController {
 
-	public generateManagerReport() {
+	public void generateManagerReport() {
 		//open selected report
 			//select report type
 				//select which report
 					//?generate?
 	}
 	
-	public generateAllReports() {
+	public void generateAllReports() {
 		//open all reports individuallly
 			//repeat steps as above, for each report
 	}
