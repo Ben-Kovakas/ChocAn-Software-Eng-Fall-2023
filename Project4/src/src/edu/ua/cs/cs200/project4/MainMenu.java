c9f099c989eb35babb26609902c99537eadd1b29
sirconnerhall <cchall5@crimson.ua.edu>
2023-11-12 16:16:52 -0500

    finished up the main menu and timer classes

--- Project4/src/src/edu/ua/cs/cs200/project4/MainMenu.java
+++ Project4/src/src/edu/ua/cs/cs200/project4/MainMenu.java
@@ -1,6 +1,10 @@
 package edu.ua.cs.cs200.project4;
 
+import java.util.Scanner;
 
+/**
+ * @author cchall5
+ */
 public class MainMenu {
 	
 	//class objects
@@ -10,29 +14,71 @@ public class MainMenu {
     Timer timer;
     SystemRecords systemRecords;
 	
-	public static void main(String[] args) {
-		//TODO - fill in main method
+	public void main(String[] args) {
+		
+		//initialize the program
+		MainMenu mainMenu;
+		mainMenu.initialize();
+		
+		//display login screen
+		System.out.println("Welcome to the ChocAn system!");
+		System.out.println("Please choose an option:");
+		System.out.println("1. Provider Terminal");
+		System.out.println("2. Operator Terminal");
+		System.out.println("3. Manager Terminal");
+		System.out.println("4. Run Main Accounting Procedure");
+		System.out.println("5. Exit");
+		
+		//read in an integer from user
+		int input = 0;
+		Scanner s = new Scanner(System.in);
+		while(input < 1 || input > 5) {
+			input = s.nextInt();
+		}
+
+		//logic for each option
+		switch(input) {
+			case 1:
+				//provider terminal
+				this.providerTerminal.main();
+				break;
+			case 2:
+				//operator terminal
+				this.operatorTerminal.main();
+				break;
+			case 3:
+				//manager terminal
+				this.managerTerminal.main();
+				break;
+			case 4:
+				//run main accounting procedure
+				this.timer.RunProcedure();
+				break;
+			case 5:
+				//exit
+				this.shutdown();	
+				break;
+		}
+		
     }
 	
 	public void initialize() {
-    	//TODO - logic to initialize program
+		
+		//TODO - persistence logic?
+		
+		//initialize class objects
+		operatorTerminal = new OperatorTerminal();
+		managerTerminal = new ManagerTerminal();
+		providerTerminal = new ProviderTerminal();
+		systemRecords = new SystemRecords();
+		timer = new Timer();
+		
     }
 
     public void shutdown() {
     	//TODO - logic to shutdown program - persistence?
-    }
 
-    public boolean login(int IDNumber, String password) {
-    	//TODO - login logic
-    	
-    	
-    	
-        return false;
-    }
-
-    public void logout() {
-    	//TODO - logout logic
-    	
+		System.out.println("Goodbye!");
     }
     
 }
--- Project4/src/src/edu/ua/cs/cs200/project4/Timer.java
+++ Project4/src/src/edu/ua/cs/cs200/project4/Timer.java
@@ -2,9 +2,11 @@ package edu.ua.cs.cs200.project4;
 
 public class Timer {
 	
+	ReportController reportController;
+	
 	public void RunProcedure() {
 		
-		ReportController reportController = new ReportController();
+		reportController = new ReportController();
 		reportController.generateAllReports();
 		
 	}
