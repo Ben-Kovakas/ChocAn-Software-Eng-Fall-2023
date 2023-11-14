ee2e64cca65f45f327920c682d3fd094302e3a88
arsteffey <arsteffey@crimson.ua.edu>
2023-11-14 12:48:19 -0600

    Updates

--- Project4/src/src/edu/ua/cs/cs200/project4/ProviderController.java
+++ Project4/src/src/edu/ua/cs/cs200/project4/ProviderController.java
@@ -1,6 +1,32 @@
 package edu.ua.cs.cs200.project4;
 
 public class ProviderController {
-    public ProviderDirectory providerDirectory;
-    public ServiceRecords serviceRecords;
+    private ProviderDirectory providerDirectory;
+    private ServiceRecords serviceRecords;
+    private List<Provider> providers;
+    private List<Member> members;
+    
+    public ProviderController(ProviderDirectory providerDirectory, ServiceRecords serviceRecords) {
+    	this.providerDirectory = providerDirectory;
+    	this.serviceRecords = serviceRecords;
+    	this.providers = providers;
+    	this.members = members;
+    }
+    
+    public boolean validateMember(int memberID) {
+    	//Search members by memberID
+    	getStatus();
+    }
+    
+    public boolean billChocAn(int memberID) {
+    	validateMember(memberID);
+    	System.out.println("Enter the date of service (MM-DD-YYYY)");
+    	//Search Provider Directory for code
+    	System.out.println("Enter the service code");
+    	//Search and Print service
+    }
+    
+    public requestProviderDirectory() {
+    	return providerDirectory.getDirectory();
+    }
 }
