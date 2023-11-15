fc777522b3e134f96abe1f7690b78c689f72eebf
JTCaldwell <jtcaldwell2@crimson.ua.edu>
2023-11-15 14:03:15 -0600

    New

--- Project4/src/src/edu/ua/cs/cs200/project4/ReportController.java
+++ Project4/src/src/edu/ua/cs/cs200/project4/ReportController.java
@@ -6,24 +6,64 @@ public class ReportController {
 		
 	}
 	
-	public void generateProviderReport() {
+	//maybe add version for ID number
+	public ProviderReport generateProviderReport(String name) {
 		//find Provider
 			//use Provider object to create report using report class
 				//return report
+		Provider tempProvider;
+		ProviderReport tempReport;
+		boolean checker;
+		//Providers: provider list
+		for(int i = 0, i </*providerRecords list size*/; i++) {
+			if(providers.get(i).getName() == name) {
+				tempProvider = providers.get(i);
+				checker = true;
+			}
+		}
+		
+		if(checker) {
+			tempReport = new ProviderReport(tempProvider);
+			return tempReport;
+		} else {
+			//maybe output saying provider not found?
+			return null;	
+		}
+		
+		
 	}
 	
 	public void generateMemberReport(String name) {
 		//find Member
 			//use Member object to create report using report class
 				//return report
+		Member tempMember;
+		MemberReport tempReport;
+		boolean checker;
+		//Members: member list
+		for(int i = 0; i < /*memberRecords list size*/; i++) {
+			if(members.get(i).getName() == name) {
+				tempMember = members.get(i);
+				checker = true;
+			}
+		}
+		
+		if(checker) {
+			tempReport = new MemberReport(tempMember);
+			return tempReport;
+		} else {
+			return null;
+		}
 	}
 	
 	public void generateAllReports() {
 		//open all reports individuallly
 			//repeat steps as above, for each report
-		generateManagerReport(1);
-		generateManagerReport(2);
-		generateManagerReport(3);
+			//Maybe return list of reports?
+		
+		for(int i = 0, i </*providerRecords list size*/; i++) {
+			
+		}
 	}
 	
 }
