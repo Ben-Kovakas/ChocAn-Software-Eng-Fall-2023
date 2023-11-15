7af6095196e20def69e2d8ab797b86e24381987b
rmmilsap <rmmilsap@crimson.ua.edu>
2023-11-15 13:22:57 -0600

    Changed ManagerTerminal to now go with new ReportController - has menu as well

--- Project4/src/src/edu/ua/cs/cs200/project4/ManagerTerminal.java
+++ Project4/src/src/edu/ua/cs/cs200/project4/ManagerTerminal.java
@@ -1,37 +1,58 @@
 package edu.ua.cs.cs200.project4;
 
-import java.util.List;
+import java.util.Scanner;
 
 public class ManagerTerminal {
 
     private ReportController reportController;
 
     public ManagerTerminal() {
-      reportController = new ReportController();
+        reportController = new ReportController();
     }
+
     public void main() {
-
-    }
-    public void requestReports() {
-        if (reportController == null) {
-            System.out.println("Report Controller is not initialized.");
-            return;
-        }
-        // check the reportController status or existence here
-        List<Report> reports = reportController.generateManagerReport();
-        displayReports(reports);
+        requestReport(); 
     }
 
-    public void displayReports(List<Report> reports) {
-        if (reports == null || reports.isEmpty()) {
-            System.out.println("No reports to display.");
-            return;
-        }
+    public void requestReport() {
+        Scanner scanner = new Scanner(System.in);
+        int input = 0;
 
-        for (Report report : reports) {
-            System.out.println(report.toString());
-        }
+        do {
+            System.out.println("\n--- Manager Terminal ---");
+            System.out.println("Please choose an option:");
+            System.out.println("1. Generate Summary Report");
+            System.out.println("2. Generate Provider Report");
+            System.out.println("3. Generate Member Report");
+            System.out.println("4. Generate EFT Report");
+            System.out.println("5. Generate All Reports");
+            System.out.println("6. Exit");
+
+            System.out.print("Enter choice: ");
+            input = scanner.nextInt();
+
+            switch (input) {
+                case 1:
+                    reportController.generateSummaryReport();
+                    break;
+                case 2:
+                    reportController.generateProviderReport();
+                    break;
+                case 3:
+                    reportController.generateMemberReport();
+                    break;
+                
+                case 4:
+                    reportController.generateAllReports();
+                    break;
+                case 5:
+                    System.out.println("Exiting Manager Terminal.");
+                    break;
+                default:
+                    System.out.println("Invalid option. Please try again.");
+            }
+        } while (input != 5);
+
+      
     }
-    
-    // NOTES: THE REPORT IS A PLACEHOLDER UNTIL FIGURED OUT
 }
