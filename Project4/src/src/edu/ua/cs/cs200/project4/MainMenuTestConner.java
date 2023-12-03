45170a10d7ef9a2cece91727fb35ed4a32eb0150
sirconnerhall <cchall5@crimson.ua.edu>
2023-12-03 14:33:08 -0600

    writing main menu test

--- Project4/src/src/edu/ua/cs/cs200/project4/MainMenuTestConner.java
+++ Project4/src/src/edu/ua/cs/cs200/project4/MainMenuTestConner.java
@@ -1,7 +1,13 @@
 package edu.ua.cs.cs200.project4;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -12,14 +18,41 @@ public class MainMenuTestConner {
 
 	private MainMenu mainMenu;
 	
-	@Before
-	public void setUp() throws Exception {
-		mainMenu = new MainMenu();
-	}
+	private final PrintStream originalOut = System.out;
+    private final InputStream originalIn = System.in;
+    private ByteArrayOutputStream outContent;
+    private ByteArrayInputStream inContent;
 
-	@Test
-	public void test() {
-		
-	}
+    @Before
+    public void setUpStreams() {
+        outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setIn(originalIn);
+    }
+
+    private void provideInput(String data) {
+        inContent = new ByteArrayInputStream(data.getBytes());
+        System.setIn(inContent);
+    }
+
+    @Test
+    public void testMainMethod() {
+        // Simulate user input (e.g., choosing option 5 to exit)
+        provideInput("5\n");
+
+        // Call the main method
+        MainMenu.main(new String[]{});
+
+        // Verify the output
+        String output = outContent.toString();
+        assertTrue(output.contains("Welcome to the ChocAn System!"));
+        assertTrue(output.contains("Please choose an option:"));
+        assertTrue(output.contains("Goodbye!"));
+    }
 
 }
