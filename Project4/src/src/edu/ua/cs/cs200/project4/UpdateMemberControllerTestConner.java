dd844e2cfbae1cc5ae5b4cbcce17e0cc2d952f2f
sirconnerhall <cchall5@crimson.ua.edu>
2023-12-03 14:15:39 -0600

    base of UpdateMemberController Test

--- Project4/src/src/edu/ua/cs/cs200/project4/UpdateMemberControllerTestConner.java
+++ Project4/src/src/edu/ua/cs/cs200/project4/UpdateMemberControllerTestConner.java
@@ -5,15 +5,46 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * @author cchall5
+ */
 public class UpdateMemberControllerTestConner {
 
+	public UpdateMemberController testController;
+	
 	@Before
 	public void setUp() throws Exception {
+		testController = new UpdateMemberController();
 	}
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	@Test (expected = IllegalArgumentException.class)
+	public void testName() {
+		testController.addMember(42069, "thisNameIsWayTooLonggggggggggggggggg", "address", "city", "state", 12345, "status");
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void testAddress() {
+		testController.addMember(42069, "name", "thisAddressIsWayyyyyyTooLonggggggg", "city", "state", 12345, "status");
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void testCity() {
+		testController.addMember(42069, "name", "address", "thisCityNameIsWayTooooooooLonggggggg", "state", 12345, "status");
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void testState() {
+		testController.addMember(42069, "name", "address", "city", "thisStateNameIsWayTooooooLongggggggg", 12345, "status");
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void testZip() {
+		testController.addMember(42069, "name", "address", "city", "state", 123456789, "status");
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void testStatus() {
+		testController.addMember(42069, "name", "address", "city", "state", 12345, "thisStatusNameIsWayyyyyyTooLong");
 	}
 
 }
