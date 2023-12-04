724b231a186332ab1913b1ca89a7257fd33691ad
rmmilsap <rmmilsap@crimson.ua.edu>
2023-12-03 23:58:33 -0600

    first test

--- Project4/src/src/edu/ua/cs/cs200/project4/RyanUnitTests.java
+++ Project4/src/src/edu/ua/cs/cs200/project4/RyanUnitTests.java
@@ -1,14 +1,35 @@
 package edu.ua.cs.cs200.project4;
 
-import static org.junit.Assert.*;
-
+import org.junit.Before;
 import org.junit.Test;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import static org.junit.Assert.*;
 
 public class RyanUnitTests {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+    private ManagerTerminal managerTerminal;
+    private final InputStream original = System.in;
 
+    @Before
+    public void setUp() {
+        managerTerminal = new ManagerTerminal();
+    }
+
+    @Test
+    public void testRequestReportOptionOne() {
+        String input = "1\n4\n"; // User selects option 1 (Generate Summary Report) and then option 4 to exit
+        ByteArrayInputStream inContent = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inContent);
+
+        // Call the method under test
+        managerTerminal.requestReport();
+
+        // Check the output or state change if any (this is just a placeholder since the actual test might vary)
+        assertTrue("Expected condition to be true after selecting option 1", true);
+
+        System.setIn(original);
+    }
+
+    // Add more tests for other options
 }
