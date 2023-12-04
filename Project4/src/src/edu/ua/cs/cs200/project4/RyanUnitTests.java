8894c38991ed5df6669fcaabbce9603c60ffe6bc
rmmilsap <rmmilsap@crimson.ua.edu>
2023-12-03 22:49:21 -0600

    new unit test file

---
+++ Project4/src/src/edu/ua/cs/cs200/project4/RyanUnitTests.java
@@ -0,0 +1,14 @@
+package edu.ua.cs.cs200.project4;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RyanUnitTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
