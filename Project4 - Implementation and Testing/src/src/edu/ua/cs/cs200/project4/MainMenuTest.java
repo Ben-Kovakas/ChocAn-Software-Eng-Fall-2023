8c9a2693a876bf6d84866d4b6f6961b13ba996d9
sirconnerhall <cchall5@crimson.ua.edu>
2023-12-11 15:46:56 -0600

    added my 2 missing test cases

--- Project4 - Implementation and Testing/src/src/edu/ua/cs/cs200/project4/MainMenuTest.java
+++ Project4 - Implementation and Testing/src/src/edu/ua/cs/cs200/project4/MainMenuTest.java
@@ -1,22 +1,55 @@
 package edu.ua.cs.cs200.project4;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 /**
  * @author cchall5
  */
-public class MainMenuTest {
+public class MainMenuTestConner {
+	
+	private final PrintStream originalOut = System.out;
+    private final InputStream originalIn = System.in;
+    private ByteArrayOutputStream outContent;
+    private ByteArrayInputStream inContent;
 
-  @Before
-  public void setUp() throws Exception {
-  }
+    @Before
+    public void setUpStreams() {
+        outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+    }
 
-  @Test
-  public void test() {
-    fail("Not yet implemented");
-  }
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setIn(originalIn);
+    }
+
+    private void provideInput(String data) {
+        inContent = new ByteArrayInputStream(data.getBytes());
+        System.setIn(inContent);
+    }
+
+    @Test
+    public void testMainMethodExit() {
+        // option 5 to exit
+        provideInput("5\n");
+
+        MainMenu.main(new String[]{});
+
+        // Verify the output
+        String output = outContent.toString();
+        assertTrue(output.contains("Welcome to the ChocAn System!"));
+        assertTrue(output.contains("Please choose an option:"));
+        assertTrue(output.contains("Goodbye!"));
+    }
 
 }
