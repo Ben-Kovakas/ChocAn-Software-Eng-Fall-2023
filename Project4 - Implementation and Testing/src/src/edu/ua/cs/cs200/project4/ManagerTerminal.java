c1ec1381235f4b00c7faae0f895a6c84d98dab07
sirconnerhall <cchall5@crimson.ua.edu>
2023-12-12 20:53:53 -0600

    fixed manager terminal to provide only one file for each type of report

--- Project4 - Implementation and Testing/src/src/edu/ua/cs/cs200/project4/ManagerTerminal.java
+++ Project4 - Implementation and Testing/src/src/edu/ua/cs/cs200/project4/ManagerTerminal.java
@@ -25,7 +25,6 @@ public class ManagerTerminal {
 
   private static ReportController reportController;
 
-
   /**
    * Constructs a {@code ManagerTerminal} and initializes the associated {@code ReportController}.
    */
@@ -72,72 +71,80 @@ public class ManagerTerminal {
     providerReportButton.addActionListener(new ActionListener() {
       @Override
       public void actionPerformed(ActionEvent e) {
-        JPanel panel1 = new JPanel(new GridLayout(0, 2));
+        // JPanel panel1 = new JPanel(new GridLayout(0, 2));
 
-        JTextField providerIDField = new JTextField();
+        // JTextField providerIDField = new JTextField();
 
-        panel1.add(new JLabel("Provider ID:"));
-        panel1.add(providerIDField);
+        // panel1.add(new JLabel("Provider ID:"));
+        // panel1.add(providerIDField);
 
-        int result1 = JOptionPane.showConfirmDialog(
-            null, panel1, "Add Member", JOptionPane.OK_CANCEL_OPTION);
+        // int result1 = JOptionPane.showConfirmDialog(
+        //     null, panel1, "Add Member", JOptionPane.OK_CANCEL_OPTION);
 
-        if (result1 == JOptionPane.OK_OPTION) {
-          // User clicked OK, process the input
+        // if (result1 == JOptionPane.OK_OPTION) {
+        //   // User clicked OK, process the input
 
-          try {
-            int providerID = Integer.parseInt(providerIDField.getText());
-            if (reportController.providerRecords.getProvider(providerID) != null) {
-              reportController.generateProviderReport(providerID).printToFile();
-              JOptionPane.showMessageDialog(null, "Provider Report Generated!");
-            }
-            else {
-              throw new IllegalArgumentException();
-            }
-          } catch (IllegalArgumentException ex) {
-            // Handle invalid input or illegal argument exception
-            JOptionPane.showMessageDialog(null, "Invalid input. Please enter valid information.", "Input Error", JOptionPane.ERROR_MESSAGE);
-          }
+        //   try {
+        //     int providerID = Integer.parseInt(providerIDField.getText());
+        //     if (reportController.providerRecords.getProvider(providerID) != null) {
+        //       reportController.generateProviderReport(providerID).printToFile();
+        //       JOptionPane.showMessageDialog(null, "Provider Report Generated!");
+        //     }
+        //     else {
+        //       throw new IllegalArgumentException();
+        //     }
+        //   } catch (IllegalArgumentException ex) {
+        //     // Handle invalid input or illegal argument exception
+        //     JOptionPane.showMessageDialog(null, "Invalid input. Please enter valid information.", "Input Error", JOptionPane.ERROR_MESSAGE);
+        //   }
 
-        } else {
-          // User clicked Cancel or closed the dialog
-        }
+        // } else {
+        //   // User clicked Cancel or closed the dialog
+        // }
+
+          reportController.generateProviderReport();
+          JOptionPane.showMessageDialog(null, "Provider Report Generated!");
+
       }
     });
     
     memberReportButton.addActionListener(new ActionListener() {
       @Override
       public void actionPerformed(ActionEvent e) {
-        JPanel panel1 = new JPanel(new GridLayout(0, 2));
+        // JPanel panel1 = new JPanel(new GridLayout(0, 2));
 
-        JTextField memberIDField = new JTextField();
+        // JTextField memberIDField = new JTextField();
 
-        panel1.add(new JLabel("Member ID:"));
-        panel1.add(memberIDField);
+        // panel1.add(new JLabel("Member ID:"));
+        // panel1.add(memberIDField);
 
-        int result1 = JOptionPane.showConfirmDialog(
-            null, panel1, "Add Member", JOptionPane.OK_CANCEL_OPTION);
+        // int result1 = JOptionPane.showConfirmDialog(
+        //     null, panel1, "Add Member", JOptionPane.OK_CANCEL_OPTION);
 
-        if (result1 == JOptionPane.OK_OPTION) {
-          // User clicked OK, process the input
+        // if (result1 == JOptionPane.OK_OPTION) {
+        //   // User clicked OK, process the input
 
-          try {
-            int memberID = Integer.parseInt(memberIDField.getText());
-            if (reportController.memberRecords.getMember(memberID) != null) {
-              reportController.generateMemberReport().printToFile();
-              JOptionPane.showMessageDialog(null, "Member Report Generated!");
-            }
-            else {
-              throw new IllegalArgumentException();
-            }
-          } catch (IllegalArgumentException ex) {
-            // Handle invalid input or illegal argument exception
-            JOptionPane.showMessageDialog(null, "Invalid input. Please enter valid information.", "Input Error", JOptionPane.ERROR_MESSAGE);
-          }
+        //   try {
+        //     int memberID = Integer.parseInt(memberIDField.getText());
+        //     if (reportController.memberRecords.getMember(memberID) != null) {
+        //       reportController.generateMemberReport().printToFile();
+        //       JOptionPane.showMessageDialog(null, "Member Report Generated!");
+        //     }
+        //     else {
+        //       throw new IllegalArgumentException();
+        //     }
+        //   } catch (IllegalArgumentException ex) {
+        //     // Handle invalid input or illegal argument exception
+        //     JOptionPane.showMessageDialog(null, "Invalid input. Please enter valid information.", "Input Error", JOptionPane.ERROR_MESSAGE);
+        //   }
 
-        } else {
-          // User clicked Cancel or closed the dialog
-        }
+        // } else {
+        //   // User clicked Cancel or closed the dialog
+        // }
+
+          reportController.generateMemberReport();
+          JOptionPane.showMessageDialog(null, "Member Report Generated!");
+
       }
     });
 
